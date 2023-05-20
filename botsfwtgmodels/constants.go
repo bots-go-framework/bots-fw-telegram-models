eb59f0478a5d4dcec3bef2aa42221df00d8c0c12
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-05-20 07:47:51 +0100

    chore(ci): added renovate.json

--- botsfwtgmodels/constants.go
+++ botsfwtgmodels/constants.go
@@ -3,4 +3,5 @@ package botsfwtgmodels
 const (
 	// BotUserCollection holds name of a "table" to Telegram user data
 	BotUserCollection = "TgBotUser" // TODO: Change value to "BotTgUser" (and move into DAL?)
+	
 )
